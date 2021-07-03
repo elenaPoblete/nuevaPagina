335813df103f06459f195dad8c1bbd0d51e28bf1
elenaPoblete <elena.poblete@gmail.com>
2021-07-03 12:18:32 -0400

    coemntario 1

---
+++ js/funciones.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.tagline', {
+    strings: ["Freelancing", "Desarrollo Web", "Emprendimiento", "Obtener un empleo en TI", "Consejos y Tips de Programación", "WordPress", "Aprender Programación", "Diseño Web", "Branding Personal"],
+    typeSpeed: 80,
+    loop: true,
+    backSpeed: 50
+});
+
+// var typed = new Typed('.element', {
+//     strings: ["Freelancing", "Desarrollo Web", "Emprendimiento", "Obtener un empleo en TI", 
+//     "Consejos y Tips de Programación", "WordPress", "Aprender Programación", "Diseño Web", 
+//     "Branding Personal"],
+
+//     smartBackspace: true // Default value
+//   });
